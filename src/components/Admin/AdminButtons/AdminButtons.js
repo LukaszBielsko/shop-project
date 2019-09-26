ac9bb7cfae6607db1a28a849087c4f58eee995a5
Lukasz Walczak <lukeabove@gmail.com>
2019-09-26 14:50:12 +0200

    ADD: dummy modal

--- src/components/Admin/AdminButtons/AdminButtons.js
+++ src/components/Admin/AdminButtons/AdminButtons.js
@@ -1,18 +1,63 @@
 import React, { Component } from 'react'
+import Modal from 'react-modal';
+
+const customStyles = {
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)'
+    }
+};
+
+Modal.setAppElement('#root');
 
 class AdminButtons extends Component {
 
-
-    checkId = (id) => {
-        console.log(id)
+    state = {
+        modalIsOpen: false,
     }
 
+    openModal = () => {
+        this.setState({ modalIsOpen: true });
+    }
+
+    afterOpenModal = () => {
+        // references are now sync'd and can be accessed.
+        this.subtitle.style.color = '#f00';
+    }
+
+    closeModal = () => {
+        this.setState({ modalIsOpen: false });
+    }
+
+
     render() {
-      
+
         return (
             <>
-                <button>edit</button>
-                <button onClick={() => this.props.remove(this.props.product.id) }>remove</button>
+                <button onClick={this.openModal}>EDIT</button>
+                <Modal
+                    isOpen={this.state.modalIsOpen}
+                    onAfterOpen={this.afterOpenModal}
+                    style={customStyles}
+                    contentLabel="Edit "
+                >
+
+                    <h2 ref={subtitle => this.subtitle = subtitle}>Hello</h2>
+                    <button onClick={this.closeModal}>close</button>
+                    <div>{this.props.product.name}</div>
+                    <form>
+                        <input name='product-name' value={this.props.product.name} />
+                        <button>tab navigation</button>
+                        <button>stays</button>
+                        <button>inside</button>
+                        <button>the modal</button>
+                    </form>
+                </Modal>
+                <button onClick={() => this.props.remove(this.props.product.id)}>remove</button>
             </>
         )
     }
