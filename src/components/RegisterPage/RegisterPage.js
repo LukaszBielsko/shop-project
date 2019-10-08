0f3f97ccab64b0b0568c1f8b8385232c5c3242a5
Lukasz Walczak <lukeabove@gmail.com>
2019-10-08 08:42:34 +0200

    ADD: registration form without logic
    Contractor can input their data into the form
    and select company they work for.

--- src/components/RegisterPage/RegisterPage.js
+++ src/components/RegisterPage/RegisterPage.js
@@ -1,9 +1,63 @@
-import React, { Component} from 'react'
+import React, { Component } from 'react'
 
+// create a form 
+// register with email and password
+// save user to db (with user uid, company, role etc)
 class RegisterPage extends Component {
-    render(){
-        return(
-            <h3>Register page under construction....</h3>
+
+    state = {
+        firstName: null,
+        lastName: null,
+        email: null,
+        password: null,
+        company: ['company-1', 'company-2']
+    }
+
+    handleInput = (event) => {
+        return null
+    }
+
+    render() {
+        return (
+            <>
+                <h4>Pls enter your details</h4>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        First Name:
+                        <input
+                            type="text" name='firstName'
+                            onChange={this.handleInput}
+                            required />
+                    </label>
+                    <label>
+                        Last Name:
+                        <input
+                            type="text" name='lastName'
+                            onChange={this.handleInput}
+                            required />
+                    </label>
+                    <select>
+                        <option value="company-1">company-1</option>
+                        <option value="company-2">company-2</option>
+                    </select>
+                    <label>
+                        email:
+                        <input
+                            type="text" name='email'
+                            onChange={this.handleInput}
+                            required />
+                    </label>
+                    <label>
+                        Password:
+                        <input
+                            type="password"
+                            name='password'
+                            onChange={this.handleInput}
+                            required />
+                    </label>
+                    <input type="submit" value="Register" />
+                </form>
+            </>
         )
     }
 }
