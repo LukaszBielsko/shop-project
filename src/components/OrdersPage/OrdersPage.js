40f9ed7066ce13d92c9a94b870cfdc445677f07b
Lukasz Walczak <lukeabove@gmail.com>
2019-10-11 11:22:03 +0200

    FIX: moved order list to render method
    Removed assigning order list to variable and
    moved it directly to render method.
    
    Arek's advice: If you do not change the
    reference of the variable then use const.
    Also extracting this orders.map to variable
    makes visual noise, as part of the rendering
    is "floating" above the actual return.
    Consider making ordersList class method or
    inlining it in line 28.

--- src/components/OrdersPage/OrdersPage.js
+++ src/components/OrdersPage/OrdersPage.js
@@ -8,27 +8,31 @@ class Orders extends Component {
     render() {
         const { orders, isAdmin, realiseOrder } = this.props;
 
-        let ordersList = orders.map((order) => {
-            return (
-                <div className={classes.Order} key={order.orderID}>
-                    <h4>Order ref: {order.orderID} </h4>
-                    <h5> Created by {order.createdBy} on {order.orderDate}</h5>
-                    <h5>Status: {order.status}</h5>
-                    {isAdmin ? <button onClick={() => realiseOrder(order.orderID, order.company)} >Realise</button> : null}
-                    <ProductsTable
-                        products={order.order}
-                        title="Ordered products" />
-                    <h5>Summary price: {order.summaryPrice}</h5>
-                </div>
-            )
-        })
-
         return (
-            <div>
-                {ordersList}
-            </div>
+            <>
+                {orders.map((order) => {
+                    return ( 
+                        <div className={classes.Order} key={order.orderID}>
+                            <h4>Order ref: {order.orderID} </h4>
+                            <h5> Created by {order.createdBy} on {order.orderDate}</h5>
+                            <h5>Status: {order.status}</h5>
+                            {isAdmin &&
+                                <button 
+                                onClick={() => realiseOrder(order.orderID, order.company)}>
+                                    Realise
+                                </button>}
+                            <ProductsTable
+                                products={order.order}
+                                title="Ordered products" />
+                            <h5>Summary price: {order.summaryPrice}</h5>
+                        </div>
+                        )
+                })
+                }
+            </>
         )
     }
+
 }
 
 export default Orders;
