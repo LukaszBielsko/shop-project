4a19352e597879bebecb31efc58b9b950516df58
Lukasz Walczak <lukeabove@gmail.com>
2019-10-17 16:57:24 +0200

    Firebase details added.

---
+++ src/FirebaseDetails.js
@@ -0,0 +1,11 @@
+const config = {
+    apiKey: "AIzaSyBcIxTYndA5uaF-vDxN2xtPQylfiIeoCSg",
+    authDomain: "fax-n-print.firebaseapp.com",
+    databaseURL: "https://fax-n-print.firebaseio.com",
+    projectId: "fax-n-print",
+    storageBucket: "",
+    messagingSenderId: "839996486243",
+    appId: "1:839996486243:web:771aec3cfadc7c19"
+};
+  
+export default config;
